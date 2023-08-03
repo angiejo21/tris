ddc0a229479a5ac1302163fdc84944f2a0123d86
Angela Bellò <angela.bello90@gmail.com>
2023-08-03 18:42:36 +0200

    + js: random names/marks player-switch msg render

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const btnNewGame = document.getElementById("new-game");
 const cellsIDs = [...cells].map((cell) => cell.id);
 
 const overlay = document.querySelector(".overlay");
+const intro = overlay.querySelector(".intro");
+const message = overlay.querySelector(".message");
 const btn1player = document.getElementById("btn1player");
 const btn2players = document.getElementById("btn2players");
 const btnStart = document.getElementById("btnStart");
@@ -17,25 +19,28 @@ const inputColorP1 = document.getElementById("inputColorP1");
 const inputMarkP0 = document.getElementById("inputMarkP0");
 const inputMarkP1 = document.getElementById("inputMarkP1");
 const optionsMarkP0 = [...inputMarkP0.querySelectorAll(`input[type="radio"]`)];
+const optionsLabelsP0 = inputMarkP0.querySelectorAll("label");
 const optionsMarkP1 = [...inputMarkP1.querySelectorAll(`input[type="radio"]`)];
+const optionsLabelsP1 = inputMarkP1.querySelectorAll("label");
 
 let players = [];
+let firstPlayer = players[0];
 let currentPlayer;
 let selectedCells = [];
 let isPlaying = true;
 
 const player0 = {
-  name: "Anonymous0",
+  name: randomPlayer(),
   game: [],
   victories: 0,
-  mark: "🥵",
+  mark: randomMark(),
   color: "#ffbe0b",
 };
 const player1 = {
-  name: "Anonymous1",
+  name: randomPlayer(),
   game: [],
   victories: 0,
-  mark: "🤢",
+  mark: randomMark(),
   color: "#3a86ff",
 };
 const player2 = {
@@ -58,6 +63,20 @@ const winningGames = [
 
 currentPlayer = players[0];
 /////////////////FUNCTIONS/////////////////
+function init() {
+  optionsMarkP0.forEach((radioOp, i) => {
+    const mark = randomMark();
+    radioOp.defaultValue = mark;
+    optionsLabelsP0[i].textContent = mark;
+  });
+  optionsMarkP1.forEach((radioOp, i) => {
+    const mark = randomMark();
+    radioOp.defaultValue = mark;
+    optionsLabelsP1[i].textContent = mark;
+  });
+}
+init();
+
 function randomSelection() {
   const freeCells = cells.filter((sCell) => !selectedCells.includes(sCell));
   console.log(freeCells);
@@ -66,6 +85,49 @@ function randomSelection() {
   currentPlayer.game.push(freeCells[index]);
   chosenCell.textContent = currentPlayer.mark;
 }
+
+function randomPlayer() {
+  const characters = [
+    "Maria Stuarda",
+    "Nemo",
+    "Barbie",
+    "Elisabetta I",
+    "Malfoy",
+    "Dory",
+    "Oppenheimer",
+    "Hermione",
+    "Sauron",
+    "Samwise",
+  ];
+  return characters[Math.floor(Math.random() * characters.length)];
+}
+
+function randomMark() {
+  const marks = [
+    "👽",
+    "🆒",
+    "👾",
+    "🌈",
+    "🥵",
+    "🤢",
+    "🧁",
+    "💩",
+    "🤧",
+    "🧜",
+    "😍",
+    "😂",
+    "🧐",
+    "👻",
+    "🐦",
+    "🐍",
+    "😾",
+    "🐶",
+    "🦑",
+    "🐿️",
+  ];
+  return marks[Math.floor(Math.random() * marks.length)];
+}
+
 function newGame() {
   cells.forEach((cell) => {
     cell.textContent = "";
@@ -74,8 +136,14 @@ function newGame() {
   selectedCells = [];
   players[0].game = [];
   players[1].game = [];
-  currentPlayer = players[0];
+  firstPlayer === players[0]
+    ? (firstPlayer = players[1])
+    : (firstPlayer = players[0]);
+  currentPlayer = firstPlayer;
   isPlaying = true;
+  overlay.style.opacity = 0;
+  message.classList.add("hidden");
+  overlay.classList.add("hidden");
 }
 /////////////////EVENTS/////////////////
 game.addEventListener("click", (e) => {
@@ -96,16 +164,15 @@ game.addEventListener("click", (e) => {
     });
     if (hasWon) {
       const index = players.indexOf(currentPlayer);
-      document.querySelector(`.player-wins--${index}`).innerHTML =
-        ++currentPlayer.victories;
-      console.log(`${currentPlayer.name} has won! 🥳`);
+      document.querySelector(
+        `.player-wins--${index}`
+      ).innerHTML = `<span>🏆</span> ${++currentPlayer.victories}`;
+      renderMessage("victory", currentPlayer);
       isPlaying = false;
-      btnNewGame.classList.toggle("hidden");
     }
     if (!hasWon && selectedCells.length === cellsIDs.length) {
-      console.log(`It's a draw 😢`);
+      renderMessage("draw", currentPlayer);
       isPlaying = false;
-      btnNewGame.classList.toggle("hidden");
     }
     if (currentPlayer === player0) {
       currentPlayer = players[1];
@@ -115,44 +182,67 @@ game.addEventListener("click", (e) => {
   }
 });
 
+function renderMessage(occurance, player) {
+  const text = document.querySelector(".message__text");
+  switch (occurance) {
+    case "victory":
+      text.innerHTML = `<h2>${player.name} ha vinto! 🎉</h2>`;
+      break;
+    case "draw":
+      text.innerHTML = `
+      <h2>Pareggio 😐</h2>
+      <h3>biscottino?🥠</h3>`;
+      break;
+  }
+  overlay.style.opacity = 100;
+  intro.classList.add("hidden");
+  message.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
 btnNewGame.addEventListener("click", newGame);
 
 btn1player.addEventListener("click", (e) => {
   e.preventDefault();
   players = [player0, player2];
-  form.classList.remove("hidden");
   formP1.style.display = "none";
+  form.classList.remove("hidden");
+  document.querySelector(".intro__buttons").classList.add("hidden");
 });
 btn2players.addEventListener("click", (e) => {
   e.preventDefault();
   players = [player0, player1];
   form.classList.remove("hidden");
+  document.querySelector(".intro__buttons").classList.add("hidden");
 });
+
 btnStart.addEventListener("click", (e) => {
   e.preventDefault();
-  player0.name = inputNameP0.value;
-  player0.color = inputColorP0.value;
+  player0.name = inputNameP0.value || player0.name;
+  player0.color = inputColorP0.value || player0.color;
   player0.mark = optionsMarkP0.filter((radio) => radio.checked)[0].defaultValue;
   if (players.includes(player1)) {
-    player1.name = inputNameP1.value;
-    player1.color = inputColorP1.value;
+    player1.name = inputNameP1.value || player1.name;
+    player1.color = inputColorP1.value || player1.color;
     player1.mark = optionsMarkP1.filter(
       (radio) => radio.checked
     )[0].defaultValue;
   }
-  rendergame(players);
+  renderGame(players);
   newGame();
 });
 
-function rendergame(arrPlayers) {
+function renderGame(arrPlayers) {
   document.querySelector(".player-mark--0").innerHTML = arrPlayers[0].mark;
   document.querySelector(".player-name--0").innerHTML = arrPlayers[0].name;
-  document.querySelector(".player-wins--0").innerHTML = arrPlayers[0].victories;
+  document.querySelector(".player-wins--0").innerHTML =
+    "🏆" + arrPlayers[0].victories;
   document.querySelector(".container-player--0").style.backgroundColor =
     arrPlayers[0].color + "aa";
   document.querySelector(".player-mark--1").innerHTML = arrPlayers[1].mark;
   document.querySelector(".player-name--1").innerHTML = arrPlayers[1].name;
-  document.querySelector(".player-wins--1").innerHTML = arrPlayers[1].victories;
+  document.querySelector(".player-wins--1").innerHTML =
+    "🏆" + arrPlayers[1].victories;
   document.querySelector(".container-player--1").style.backgroundColor =
     arrPlayers[1].color + "aa";
   overlay.style.opacity = 0;
